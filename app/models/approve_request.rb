35c30bd9ed055e74c6a81655ec562287a2dc6c31
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2019-12-11 09:20:27 +0900

    파트너센터/ 업체 승인 API 추가

--- app/models/approve_request.rb
+++ app/models/approve_request.rb
@@ -1,7 +1,41 @@
 class ApproveRequest < ApplicationRecord
   enum status: %i[pending rejected accepted]
-  belongs_to :approvable
+  belongs_to :approvable, polymorphic: true
 
   scope :alive, -> { where(alive: true) }
   scope :dead, -> { where(alive: false) }
+
+  before_create :set_timestamp
+
+  def before
+    before_all.last
+  end
+
+  def before_all
+    self.class.where(approvable: approvable).where.not(id: id)
+  end
+
+  def alive_before
+    before_all.alive
+  end
+
+  def dead_before!
+    alive_before.update_all(alive: false)
+  end
+
+  def rollback!
+    _before = before
+    transaction do
+      destroy
+      _before.update(alive: true)
+    end
+  end
+
+  private
+
+  def set_timestamp
+    now = Time.zone.now
+    self.created_at = now
+    self.updated_at = now
+  end
 end
