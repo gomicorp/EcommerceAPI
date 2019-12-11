2c815dc60e46d04dd5e8b2ca58f200ec8500d85b
Bran <june20516@gmail.com>
2019-12-11 09:46:19 +0900

    locale 정보 설정

--- config/initializers/locale_router.rb
+++ config/initializers/locale_router.rb
@@ -2,7 +2,7 @@
 
 LocaleRouter.configuration(Rails) do |config|
   config.available_locales = %w[en ko th]
-  config.default_locale = :th
+  config.default_locale = :ko
   config.auto_follow_access_header = false
   config.auto_prepend_before_action = false
   config.locale_param_matcher = :week unless Rails.env.production?
