08f5849994c656c1a5dd0875d5b285c412c54279
DONGWON KIM <lunacircle4@likelion.org>
2019-12-23 12:08:38 +0900

    리펙토링: "type" => "query" 로 이름 변경

--- app/controllers/gomisa/adjustments_controller.rb
+++ app/controllers/gomisa/adjustments_controller.rb
@@ -9,27 +9,26 @@ module Gomisa
       private
       # Use callbacks to share common setup or constraints between actions.
       def filter_adjustments
-        type = action_type(params[:reason])
-        if type == "All" 
+        query = reason(params[:reason])
+        if query == "All" 
           return from_to_date_filter(
             Adjustment,
             params[:from], params[:to]
           )
-        else
-          return from_to_date_filter(
-            Adjustment.where(
-              reason: type
-            ),
-            params[:from], params[:to]
-          )
         end
+        return from_to_date_filter(
+          Adjustment.where(
+            reason: query
+          ),
+          params[:from], params[:to]
+        )
       end
 
       def from_to_date_filter(queryset, from, to)
         return queryset.where(exported_at: (from.to_date..to.to_date))
       end
 
-      def action_type(reason)
+      def reason(reason)
         # 수정하기
         # 로직을 이렇게 짜서 if else 가 안길어지게 하자
         # {
