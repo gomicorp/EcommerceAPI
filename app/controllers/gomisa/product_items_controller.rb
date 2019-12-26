161849e921d516b9dd0da2542136c205361e4506
DONGWON KIM <lunacircle4@likelion.org>
2019-12-26 12:04:25 +0900

    리펙토링: product_item을 위한 index request 로직 분리

--- app/controllers/gomisa/product_items_controller.rb
+++ app/controllers/gomisa/product_items_controller.rb
@@ -2,7 +2,10 @@ module Gomisa
   class ProductItemsController < BaseController
     # GET /gomisa/product_items.json
     def index
-      @product_items = ProductItem.all
+      @service = ProductItemIndexService.new(params[:from], params[:to], params[:channel])
+      @service.call
+      @product_items = @service.product_items
+      render json: @product_items
     end
   end
 end
