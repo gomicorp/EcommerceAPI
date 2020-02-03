7304aa0a5e237b658141a24b77b44fb1d70b1e2d
DONGWON KIM <33149791+lunacircle4@users.noreply.github.com>
2020-02-03 15:52:28 +0900

    기능: bridges(connectables)만 이용해서 사용

--- app/models/product_option.rb
+++ app/models/product_option.rb
@@ -3,10 +3,11 @@ class ProductOption < ApplicationRecord
   has_many :barcode_options
   has_many :barcodes, through: :barcode_options
 
-  has_many :product_option_bridges
-  has_many :items, :through => :product_option_bridges, :source => :connectable, :source_type => 'ProductItem'
-  has_many :collections, :through => :product_option_bridges, :source => :connectable, :source_type => 'ProductCollection'
-  has_many :product_item_barcodes, through: :items
+  has_many :bridges, class_name: 'ProductOptionBridge'
+  #has_many :items, through: :product_option_bridges, source: :connectable, source_type: 'ProductItem'
+  #has_many :collections, through: :product_option_bridges, source: :connectable, source_type: 'ProductCollection'
+
+  #has_many :product_item_barcodes, through: :items
 
   delegate :product, to: :option_group
   def title
@@ -21,27 +22,20 @@ class ProductOption < ApplicationRecord
     @retail_price ||= base_price + price_change
   end
 
-  # connectables getter
-  def connectables
-    items + collections
-  end
-
-  # connectables setter
-  def connectables=(connectable_list)
-    new_items = []
-    new_collections = []
-
-    connectable_list.each do |connectable|
-      if connectable.class == ProductItem
-        new_items.append(connectable)
-      else
-        new_collections.append(connectable)
-      end
-    end
-
-    items << new_items
-    collections << new_collections
-  end
+  ## connectables getter
+  #def connectables
+  #  items + collections
+  #end
+  #
+  ## connectables setter
+  #def connectables=(connectable_list)
+  #  connectable_list.each do |connectable|
+  #    case connectable
+  #    when ProductItem        then items << connectable
+  #    when ProductCollection  then collections << connectable
+  #    end
+  #  end
+  #end
 
   # TODO: 할인/추가 금액 관련. 리펙토링 필요.
   def price_change
