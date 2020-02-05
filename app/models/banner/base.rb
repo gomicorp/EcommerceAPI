009f5d7885596ecf02b11ff3e3e8b1ab96714617
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2020-02-06 05:00:40 +0900

    기능: 배너에 이미지 필드 추가

--- app/models/banner/base.rb
+++ app/models/banner/base.rb
@@ -2,6 +2,7 @@ module Banner
   class Base < ApplicationRecord
     self.table_name = :banners
     enum banner_type: %i[home category]
+    extend_has_one_attached :image
 
     scope :active, -> { where(active: true) }
   end
