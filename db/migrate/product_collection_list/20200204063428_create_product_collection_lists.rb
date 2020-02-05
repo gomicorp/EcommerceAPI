ffd0ec47edcbe649743533b51337c3ad75bccb78
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2020-02-05 15:54:46 +0900

    기능: ViewTableBuilder 분리

--- db/migrate/product_collection_list/20200204063428_create_product_collection_lists.rb
+++ db/migrate/product_collection_list/20200204063428_create_product_collection_lists.rb
@@ -1,25 +1,29 @@
 class CreateProductCollectionLists < ActiveRecord::Migration[6.0]
-  def table_name
-    'product_collection_lists'
-  end
+  include ViewTableBuilder
 
+  # CREATE VIEW #{table_name} AS
+  # SELECT
+  #   elem.product_item_id AS item_id,
+  #   elem.product_collection_id AS collection_id,
+  #   COUNT(*) as unit_count
+  # FROM product_collection_elements elem
+  # GROUP BY
+  #   elem.product_item_id,
+  #   elem.product_collection_id;
   def up
-    execute <<~SQL
-      CREATE VIEW #{table_name} AS
-      SELECT
-        product_item_id AS item_id,
-        product_collection_id AS collection_id,
-        COUNT(*) as unit_count
-      FROM product_collection_elements elem
-      GROUP BY
-        elem.product_item_id,
-        elem.product_collection_id;
-    SQL
+    create_view :product_collection_lists do |t|
+      t.from :product_collection_elements, :elem
+
+      t.group_with source_table: :elem, source_column: :product_item_id
+      t.group_with source_table: :elem, source_column: :product_collection_id
+
+      t.column :item_id,        source_table: :elem, source_column: :product_item_id
+      t.column :collection_id,  source_table: :elem, source_column: :product_collection_id
+      t.column :unit_count,     sql: 'COUNT(*)'
+    end
   end
 
   def down
-    execute <<~SQL
-      DROP VIEW #{table_name}
-    SQL
+    drop_view :product_collection_lists
   end
 end
