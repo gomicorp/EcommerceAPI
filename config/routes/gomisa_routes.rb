b78a56b8f85b33dc3e862503fb6588f1273cc105
DONGWON KIM <33149791+lunacircle4@users.noreply.github.com>
2020-02-16 23:27:52 +0900

    기능: routes.rb,  companies, brands, product_item_groups 추가

--- config/routes/gomisa_routes.rb
+++ config/routes/gomisa_routes.rb
@@ -1,12 +1,13 @@
 namespace :gomisa do
   readonly = %i[index show]
 
-  resources :companies, only: readonly do
+  resources :companies do
     resources :brands, module: :companies, only: readonly
   end
-  resources :brands, only: readonly do
+  resources :brands do
     resource :company, module: :brands, only: readonly
   end
+  resources :product_item_groups
   resources :product_items
   resources :adjustments
 
