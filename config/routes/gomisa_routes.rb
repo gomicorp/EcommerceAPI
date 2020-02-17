8ad905d897d01b79d641155bd38603b8ed95eb0f
DONGWON KIM <33149791+lunacircle4@users.noreply.github.com>
2020-02-17 16:09:36 +0900

    기능: product_item_groups, product_collections 라우팅 추가

--- config/routes/gomisa_routes.rb
+++ config/routes/gomisa_routes.rb
@@ -7,7 +7,22 @@ namespace :gomisa do
   resources :brands do
     resource :company, module: :brands, only: readonly
   end
-  resources :product_item_groups
+
+
+  resources :product_item_groups do
+    resources :items, module: :product_item_groups do
+      resources :adjustments, module: :items
+    end
+  end
+
+  # 카테고리 관리
+  resources :product_collections do
+    scope module: :product_collections do
+      resources :lists
+      resources :elements, only: %i[create destroy]
+    end
+  end
+
   resources :product_items
   resources :adjustments
 
