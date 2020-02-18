719163a8e6b4a65f3eaf205c87b9016e390067e1
DONGWON KIM <33149791+lunacircle4@users.noreply.github.com>
2020-02-18 15:50:21 +0900

    기능: Gomisa/ ProductCollection, controller 구현

--- app/controllers/gomisa/product_collections/lists_controller.rb
+++ app/controllers/gomisa/product_collections/lists_controller.rb
@@ -4,11 +4,9 @@ module Gomisa
       before_action :set_product_collection
       before_action :set_product_collection_list, only: %i[show edit update destroy]
 
+      # 현재 등록할수 있는 item들을 반환한다.
       def index
-      end
-
-      def new
-        @product_collection_list = @product_collection.lists.build(unit_count: 1)
+        @allowed_product_items = ProductItem.where.not(id: @product_collection.lists.pluck(:item_id)).pluck(:name, :id)
       end
 
       def create
@@ -21,25 +19,14 @@ module Gomisa
 
         @product_collection_list = ProductCollectionList.find(@product_collection_list.item_id)
 
-        if @product_collection_list.persisted?
-          render json: @product_collection, status: :created
-        else
-          render json: @product_collection_list.errors, status: :bad_request
-        end
-
-        #if @company.save
-        #  render json: @company, status: :created
-        #else
-        #  render json: @company.errors, status: :unprocessable_entity
-        #end
-        # 새로운 튜플이 아니거나
-        # 삭제되지 않으면 false
+        # 어짜피 위에서 저장안되면 422 status code 반환
+        render json: @product_collection, status: :created
       end
 
       def destroy
         elements = @product_collection.elements.where(product_item: @product_collection_list.item)
         @product_collection.elements.destroy(elements)
-        return redirect_back fallback_location: office_product_collection_path(@product_collection), notice: 'Collection Elements has been successfully destroyed.'
+        render json: {}, status: :no_content
       end
 
       private
