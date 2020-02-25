18447b15ff55208bc288cff2315727ffe317f7a4
JW <jjwoo93@naver.com>
2020-02-25 10:20:45 +0900

    Chore : utf8mb4를 적용하는 루비코드를 쿼리로 변경

--- db/migrate/ship_info/20200224005800_alter_column_to_utf8mb4.rb
+++ db/migrate/ship_info/20200224005800_alter_column_to_utf8mb4.rb
@@ -1,6 +1,11 @@
 class AlterColumnToUtf8mb4 < ActiveRecord::Migration[6.0]
-  def change
-    change_table(:ship_infos, collate: :utf8mb4_general_ci, charset: :utf8mb4) {}
+  def up
+    execute "ALTER TABLE ship_infos CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci"
+    change_column :ship_infos, :loc_detail, :text
+  end
+
+  def down
+    execute "ALTER TABLE ship_infos CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci"
     change_column :ship_infos, :loc_detail, :text
   end
 end
