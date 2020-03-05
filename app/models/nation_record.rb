36fb1bb5cfbcb445df74019791cabe2da1112898
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2020-03-05 14:31:19 +0900

    기능: 국가귀속모델에 대한 스토어 미러링 적용

--- app/models/nation_record.rb
+++ app/models/nation_record.rb
@@ -1,11 +1,23 @@
 class NationRecord < ApplicationRecord
   self.abstract_class = true
 
-  default_scope -> {
-    if ApplicationRecord.country_code
-      where(country: Country.find_by(short_name: ApplicationRecord.country_code))
-    end
-  }
+  belongs_to :country, optional: true
 
-  belongs_to :country
+  default_scope -> { th }
+  scope :th, -> { unscoped.left_joins(:country).where(countries: { short_name: 'th' }) }
+  scope :vn, -> { unscoped.left_joins(:country).where(countries: { short_name: 'vn' }) }
+  scope :undef, -> { unscoped.left_joins(:country).where(countries: { short_name: nil }) }
+  scope :at, ->(key) { unscoped.left_joins(:country).where(countries: { short_name: key }) }
+
+  before_save :callback_attaching_country
+
+  def self.migrate_original_record_to_thai_record
+    unscoped.where(country_id: nil).update_all(country_id: Country.th.id)
+  end
+
+  private
+
+  def callback_attaching_country
+    self.country ||= Country.th
+  end
 end
