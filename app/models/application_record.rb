8ea7bd043b77824924879c80eade18909511c2c9
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2020-03-17 16:11:52 +0900

    기능: HistoricalRecordable 전역 모델에 적용

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
-  extend AutoIncrement
   self.abstract_class = true
+  extend AutoIncrement
+  include HistoricalRecordable
 
   def cls
     self.class
@@ -16,5 +17,23 @@ class ApplicationRecord < ActiveRecord::Base
     has_many_attached name.to_sym
     scope "#{name}_attached".to_sym, -> { left_joins("#{name}_attachments".to_sym).where('active_storage_attachments.id is NOT NULL') }
     scope "#{name}_unattached".to_sym, -> { left_joins("#{name}_attachments".to_sym).where('active_storage_attachments.id is NULL') }
+  end
+
+  def self.associations
+    reflect_on_all_associations.map do |reflect|
+      {klass: reflect.klass, name: reflect.name, options: reflect.options}
+    end
+  end
+
+  def self.association_names
+    associations.pluck(:name)
+  end
+
+  def associations
+    self.class.associations
+  end
+
+  def association_names
+    self.class.association_names
   end
 end
