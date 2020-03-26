d92d9a6334b93d4fd71da803b2cc93a6df379bcd
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2020-03-26 15:27:25 +0900

    수정: 가격캐싱 마이그레이션 파일 이동 & 세트상품 가격변동 전파 누락부분 수정

--- app/models/product_collection_element.rb
+++ app/models/product_collection_element.rb
@@ -1,4 +1,15 @@
 class ProductCollectionElement < ApplicationRecord
   belongs_to :product_item
   belongs_to :collection, class_name: 'ProductCollection', foreign_key: :product_collection_id
+
+  after_create :after_save_propagation
+  after_destroy :after_save_propagation
+
+  private
+
+  def after_save_propagation
+    col = collection
+    col.calculate_price_columns
+    col.save
+  end
 end
