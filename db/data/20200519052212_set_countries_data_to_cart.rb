519ea6ed37e943f466fbed556e3e70e2b35e246c
Bran <40688044+june20516@users.noreply.github.com>
2020-05-20 10:59:08 +0900

    수정: 스모리 / cart country 데이터 마이그레이션에서 country source 보강
    cart에서 country 에 접근할 수 있는 데이터를 깊이순으로 접근하여 카트 업데이트

--- db/data/20200519052212_set_countries_data_to_cart.rb
+++ db/data/20200519052212_set_countries_data_to_cart.rb
@@ -3,9 +3,11 @@ class SetCountriesDataToCart < ActiveRecord::Migration[6.0]
     ApplicationRecord.country_context_with 'global' do
       default_country = Country.th
       Cart.unscoped.all.each do |cart|
+        country_source = cart.order_info
         cart_item = cart.items.first
-        if cart_item
-          country_source = cart_item.product_option.channel
+        country_source ||= cart_item&.product_option&.channel
+        country_source ||= cart_item&.product_item_barcodes.first&.product_item
+        if country_source
           cart.update!(country: country_source.country)
         else
           cart.update!(country: default_country)
