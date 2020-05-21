994b370251fb81830aafeba41aa808f5ead1e930
EC2 User on EcommerceAPI <tech@gomicorp.com>
2020-05-21 06:48:44 +0000

    Hotfix: Cart DataMigration Code

--- db/data/20200519052212_set_countries_data_to_cart.rb
+++ db/data/20200519052212_set_countries_data_to_cart.rb
@@ -2,11 +2,14 @@ class SetCountriesDataToCart < ActiveRecord::Migration[6.0]
   def up
     ApplicationRecord.country_context_with 'global' do
       default_country = Country.th
+
       Cart.unscoped.all.each do |cart|
         country_source = cart.order_info
         cart_item = cart.items.first
+
         country_source ||= cart_item&.product_option&.channel
-        country_source ||= cart_item&.product_item_barcodes.first&.product_item
+        country_source ||= cart_item&.product_item_barcodes&.first&.product_item
+
         if country_source
           cart.update!(country: country_source.country)
         else
