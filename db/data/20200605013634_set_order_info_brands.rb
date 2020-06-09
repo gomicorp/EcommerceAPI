7dd1535a59b9057556bc9ac916c6cf32527d5bd1
JW <jjwoo93@naver.com>
2020-06-09 11:47:09 +0900

    기능: 누락된 데이터를 생성하는 로직 추가

--- db/data/20200605013634_set_order_info_brands.rb
+++ db/data/20200605013634_set_order_info_brands.rb
@@ -9,11 +9,18 @@ class SetOrderInfoBrands < ActiveRecord::Migration[6.0]
             end
           end
         end
+
+        # 정상적인 로직으로 데이터를 채워줄 수 없는 레거시 데이터에 대한 대응
+        order_info.items.each do |item|
+          item.barcodes.each do |barcode|
+            OrderInfoBrand.create(order_info: order_info, brand: barcode.product.brand)
+          end
+        end
       end
     end
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration if Rails.production?
+    # raise ActiveRecord::IrreversibleMigration if Rails.production?
   end
 end
