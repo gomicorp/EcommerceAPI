dc9d8b416d3de08b5c429b794612e3f680cc4c1d
Bran <40688044+june20516@users.noreply.github.com>
2020-06-10 11:19:38 +0900

    country 모델 상세화 및 스토어와 일치

--- app/models/country.rb
+++ app/models/country.rb
@@ -6,8 +6,8 @@ class Country < ApplicationRecord
   validates_presence_of :name, :name_ko, :locale, :short_name
   alias_attribute :code, :short_name
 
-  TH = find_or_create_by(name: 'thailand', name_ko: '태국', locale: 'th', short_name: :th)
-  VN = find_or_create_by(name: 'vietnam', name_ko: '베트남', locale: 'vi', short_name: :vn)
+  TH = find_or_create_by(name: 'thailand', name_ko: '태국', locale: 'th', short_name: 'th', iso_code: 'THB')
+  VN = find_or_create_by(name: 'vietnam', name_ko: '베트남', locale: 'vi', short_name: 'vn', iso_code: 'VND')
   KO = find_or_create_by(name: 'korea', name_ko: '한국', locale: 'ko', short_name: 'ko', iso_code: 'KRW')
 
   def self.th
