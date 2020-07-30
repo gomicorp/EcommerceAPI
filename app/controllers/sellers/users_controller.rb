3f4b3f28648537d7a98d47ecd0f23666cd0e6c50
JW <jjwoo93@naver.com>
2020-07-30 15:54:33 +0900

    수정: 셀러즈 API / Users show, update 메소드 수정, show 뷰 파일 생성

--- app/controllers/sellers/users_controller.rb
+++ app/controllers/sellers/users_controller.rb
@@ -1,23 +1,31 @@
 module Sellers
   class UsersController < BaseController
-    before_action :set_seller, only: %i[show]
+    before_action :authenticate_user!
+    before_action :set_seller, only: %i[show update]
+    before_action :check_authorization!, only: %i[update destroy]
 
     # = GET /sellers/users
     def index; end
 
     # = GET /sellers/users/:id
     def show
-      render json: @seller
     end
 
     # = POST /sellers/users
     def create; end
 
-    # = PUT /sellers/users/:id
+    # = PATCH /sellers/users/:id
     def update
-    # 프로필 이미지 업데이트
-    # 생일, 성별 업데이트
-    #
+      # 프로필 이미지 업데이트
+      # 생일, 성별 업데이트
+      @seller.birth_day = params[:birth_day].to_datetime.to_date
+      @seller.gender = params[:gender]
+
+      if @seller.save
+        render json: @seller, status: :ok
+      else
+        render json: @seller.errors, status: :unprocessable_entity
+      end
     end
 
     # = DELETE /sellers/users/:id
@@ -27,6 +35,15 @@ module Sellers
 
     def set_seller
       @seller = Seller.find(params[:id])
+
+      raise ActiveRecord::RecordNotFound unless @seller
+    end
+
+    def check_authorization!
+      # @seller 가 먼저 선언되어있어야 합니다.
+      return true if @seller.id == current_user.id
+
+      raise ApiController::NotAuthorized
     end
   end
 end
