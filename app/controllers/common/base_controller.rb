e027ce796763790300b2dcec8c307099539e7aed
JW <jjwoo93@naver.com>
2020-08-04 18:14:36 +0900

    수정: 셀러즈 API / common API의 base 컨트롤러 생성

---
+++ app/controllers/common/base_controller.rb
@@ -0,0 +1,25 @@
+module Common
+  class BaseController < ApiController
+
+    # CRUD stuff
+    def index; end
+
+    # CRUD stuff
+    def show; end
+
+    # CRUD stuff
+    def new; end
+
+    # CRUD stuff
+    def create; end
+
+    # CRUD stuff
+    def edit; end
+
+    # CRUD stuff
+    def update; end
+
+    # CRUD stuff
+    def destroy; end
+  end
+end
---
+++ test/controllers/common/base_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Common::BaseControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
