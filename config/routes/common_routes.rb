901de359624811b8a4cb621b7345b81bb5ace8ac
JW <jjwoo93@naver.com>
2020-08-04 18:15:48 +0900

    수정: 셀러즈 API / common 라우트 수정

--- config/routes/common_routes.rb
+++ config/routes/common_routes.rb
@@ -1,7 +1,7 @@
-scope module: :common do
-  resources :sellers
-
+namespace :common, except: %i[new edit] do
   # === 관심사 태그
   #
-  resources :interest_tags
+  resources :interest_tags, controller: 'interest_tags' do
+    get '/:scope', action: :index, on: :collection, as: :scope
+  end
 end
