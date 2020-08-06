4614eeae97e948dfe9899b8b55abd7d70baf98c4
JW <jjwoo93@naver.com>
2020-08-06 14:19:30 +0900

    수정: 셀러즈 API / Seller와 Seller Info의 서비스를 리팩토링

--- app/services/sellers/seller_info_create_service.rb
+++ app/services/sellers/seller_info_create_service.rb
@@ -14,72 +14,60 @@ module Sellers
       # = 셀러 정보 생성
       @seller_info = SellerInfo.new(@seller_info_params)
 
-      # = 관심 태그 생성
-      @interest_tags = []
-      @interest_tag_params[:interest_tags].each do |tag_name|
-        @interest_tags << InterestTag.find_or_initialize_by(name: tag_name) do |interest_tag|
-          interest_tag.created_by = "user"
-        end
-      end
-
       # = 팝업 스토어 정보 생성
       @store_info = StoreInfo.new(@store_info_params)
     end
 
     def save
-      begin
-        ApplicationRecord.transaction do
-          # 0. Associations
-          @seller_info.seller = @seller
-          @seller_info.grade = Grade.first
-          @store_info.seller_info = @seller_info
+      ApplicationRecord.transaction do
+        # 0. Associations
+        @seller_info.seller = @seller
+        @seller_info.grade = Grade.first
+        @store_info.seller_info = @seller_info
 
-          # 1. save seller_info
-          valid_save @seller_info
+        # 1. save seller_info
+        @seller_info.save!
 
-          # 2. save interest_tags
-          valid_save @interest_tags
+        # 2. save interest_tags
+        save_interest_tags
 
-          # 3. save store_info
-          valid_save @store_info
-          @store_info.update(url: 'https://gomistore.in.th/popup_store/' + @store_info.id.to_s)
+        # 3. save store_info
+        @store_info.save!
+        @store_info.update!(url: 'https://gomistore.in.th/popup_store/' + @store_info.id.to_s)
 
-          # 4. update seller
-          @seller.assign_attributes(@seller_params)
-          valid_save @seller
+        # 4. update seller
+        @seller.update!(@seller_params)
 
-          # 5. create seller_info_interest_tags
-          @seller_info_interest_tags = []
-          @interest_tags.each do |interest_tag|
-            @seller_info_interest_tags << SellerInfoInterestTag.new(seller_info: @seller_info, interest_tag: interest_tag)
-          end
-          valid_save @seller_info_interest_tags
-
-          raise ActiveRecord::Rollback if @errors
-        end
+        # 5. create seller_info_interest_tags
+        @seller_info.interest_tags << @interest_tags
       end
 
-      SellerInfo.where(id: @seller_info.id).any?
+      # 이곳은 정상실행 됐을때만 실행되는 안전구역 입니다.
+      return true
+
+    rescue ActiveRecord::ActiveRecordError => e
+      # 이곳은 깨졌을 때만 실행되는 에러 핸들링 구역입니다.
+        @errors = e
+      return false
     end
 
     private
 
-    def valid_save(record)
-      if record.is_a? Array
-        record.each { |rec| rec.save! if valid_record? rec }
-      else
-        record.save! if valid_record? record
-      end
+    # = 관심 태그 생성
+    private def save_interest_tags
+      # a. create, update, destroy 는 루프를 돌면서 실행하면 많이 느리다.
+      # b. 중복된 값을 허용하면 안된다.
+      names_already_exists = InterestTag.where(name: interest_tag_params_names).pluck(:name)
+      names_to_be_create = interest_tag_params_names - names_already_exists
+      # ['', ''] => [{}, {}]
+      interest_tag_objects = names_to_be_create.map { |name| { name: name, created_by: 'user' } }
+      InterestTag.create!(interest_tag_objects)
+
+      @interest_tags = InterestTag.where(name: interest_tag_params_names)
     end
 
-    def valid_record?(record)
-      if record.invalid?
-        @errors = record.errors
-        false
-      else
-        true
-      end
+    private def interest_tag_params_names
+      @interest_tag_params[:interest_tags]
     end
-
   end
 end
--- app/services/sellers/seller_update_service.rb
+++ app/services/sellers/seller_update_service.rb
@@ -1,5 +1,7 @@
 module Sellers
   class SellerUpdateService
+    attr_reader :errors
+
     def initialize(service_params, seller)
       @seller = Seller.find seller.id
       @seller_params = service_params[:seller_params]
@@ -12,40 +14,23 @@ module Sellers
       begin
         ApplicationRecord.transaction do
           # 1. update seller
-          @seller.assign_attributes(@seller_params)
-          valid_save @seller
+          @seller.update!(@seller_params)
 
           # 2. create user_interest_tags
-          @user_interest_tags = []
-          @interest_tag_params[:interest_tags].each do |tag_name|
-            @user_interest_tags << UserInterestTag.new(user: @seller, interest_tag: InterestTag.find_by(name: tag_name))
-          end
-          valid_save @user_interest_tags
-
-          raise ActiveRecord::Rollback if @errors
+          @seller.interest_tags = InterestTag.where(name: interest_tag_params_names)
         end
       end
 
-      @seller.interest_tags.any?
+      return true
+
+    rescue ActiveRecord::ActiveRecordError => e
+      # 이곳은 깨졌을 때만 실행되는 에러 핸들링 구역입니다.
+      @errors = e
+      return false
     end
 
-    private
-
-    def valid_save(record)
-      if record.is_a? Array
-        record.each { |rec| rec.save! if valid_record? rec }
-      else
-        record.save! if valid_record? record
-      end
-    end
-
-    def valid_record?(record)
-      if record.invalid?
-        @errors = record.errors
-        false
-      else
-        true
-      end
+    private def interest_tag_params_names
+      @interest_tag_params[:interest_tags]
     end
   end
 end
