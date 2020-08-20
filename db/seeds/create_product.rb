444e19a0308c95f22f68cdb2e4a26b7d43b07fca
JW <jjwoo93@naver.com>
2020-08-20 16:51:44 +0900

    수정: 상품 생성 시드 파일 버그 수정

--- db/seeds/create_product.rb
+++ db/seeds/create_product.rb
@@ -2,19 +2,27 @@ require 'open-uri'
 Faker::Config.locale = 'en'
 
 ApplicationRecord.transaction do
-  options = ProductOptionGroup.all.select{ |a| a.options.count != 0 }
-  10000.times do
+  options = ProductOptionGroup.all.select{ |a| a.product && a.product.country == Country.th && a.options.count != 0 }
+
+  500.times do
     title = Faker::Book.title
     product = Product.new(title: { ko: title, en: title, th: title })
     cur_options = options.sample.options
-    product.brand = Brand.all.sample
+
+    brand = Brand.all.sample
+    product.brand = brand
+
     product.country = Country.th
-    product.categories << Category.all.sample
+
     product.running_status = 3
     product.default_option = cur_options.first
-    product.save
+    product.save!
 
     product.option_groups << ProductOptionGroup.new(product: product, options: cur_options)
+
+    category = Category.all.sample
+    product.categories << category
+
     download_image = open(Faker::Avatar.image)
     product.thumbnail.attach(io: download_image, filename: "thumb.png")
   end
