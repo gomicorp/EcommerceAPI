d50e2c49d4df29345bf8ca135ca6f75ec3f1c7d3
JW <jjwoo93@naver.com>
2020-08-24 11:43:31 +0900

    수정: 옵션 연결 시드파일 수정

--- db/seeds/connect_options.rb
+++ db/seeds/connect_options.rb
@@ -3,7 +3,7 @@ ApplicationRecord.transaction do
     ProductOptionGroup.select{ |a| a.product && a.product.country == Country.th && a.options.count != 0 }.sample.options.each do |option|
       new_option = option.dup
       new_option.update!(product: option_group.product)
-      option_group << new_option
+      option_group.options << new_option
     end
   end
 end
