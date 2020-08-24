0e6c3492434f25bdc9299db802c0da4344ea5b9d
JW <jjwoo93@naver.com>
2020-08-24 15:10:32 +0900

    수정: 시드파일 조건문 수정

--- db/seeds/make_connectables.rb
+++ db/seeds/make_connectables.rb
@@ -3,7 +3,7 @@ sample_item = ProductItem.select{ |item| item.available_quantity > 0 }
 sample_collection = ProductCollection.select{ |collection| collection.available_quantity > 0 }
 
 ApplicationRecord.transaction do
-  ProductOption.where(bridges: ProductOptionBridge.all).each do |option|
+  ProductOption.where.not(bridges: ProductOptionBridge.all).each do |option|
     if type.sample == "ProductItem"
       connectable = sample_item.sample
     else
