052f053f41c7b719ed631c9618a9d825449d9be2
JW <jjwoo93@naver.com>
2020-08-24 16:58:44 +0900

    수정: 시드파일 오류 수정

--- db/seeds/set_product_option_active.rb
+++ db/seeds/set_product_option_active.rb
@@ -2,6 +2,6 @@ target_options = ProductOption.inactive.where(product: Product.all)
 
 ApplicationRecord.transaction do
   target_options.each do |option|
-    option.update!(active: true)
+    option.update!(is_active: true)
   end
 end
