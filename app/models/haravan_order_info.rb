f2c0e776b904addc4e5f1e6f04f84c22ab8b9af3
JW <jjwoo93@naver.com>
2020-09-09 22:56:51 +0900

    수정: 마이그레이션 디렉토리 수정 및 HaravanOrderInfo 스키마 추가

--- app/models/haravan_order_info.rb
+++ app/models/haravan_order_info.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: haravan_order_infos
+#
+#  id               :bigint           not null, primary key
+#  ordered_at       :datetime
+#  total_price      :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  haravan_order_id :integer
+#
 class HaravanOrderInfo < ApplicationRecord
 end
--- test/models/haravan_order_info_test.rb
+++ test/models/haravan_order_info_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: haravan_order_infos
+#
+#  id               :bigint           not null, primary key
+#  ordered_at       :datetime
+#  total_price      :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  haravan_order_id :integer
+#
 require 'test_helper'
 
 class HaravanOrderInfoTest < ActiveSupport::TestCase
