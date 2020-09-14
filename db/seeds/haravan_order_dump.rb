5ac42a73e9178286c6e35b2df5e0703812393319
JW <jjwoo93@naver.com>
2020-09-14 18:53:25 +0900

    수정: DB 시드파일 수정 및 추가

--- db/seeds/haravan_order_dump.rb
+++ db/seeds/haravan_order_dump.rb
@@ -2,11 +2,11 @@ include HaravanApiHelper
 include HaravanApiHelper::Order
 
 ApplicationRecord.transaction do
-  records = self.get_order_by_period('2020-08-01T00:00:00.000Z', '2020-08-02T23:59:59.999Z')
+  records = self.get_order_by_period('2020-08-13T07:00:00.000Z', '2020-08-20T06:59:59.999Z')
 
   records.each do |record|
-    HaravanOrderInfo.create(haravan_order_id: record['id'],
-                            total_price: record['total_price'],
-                            ordered_at: record['created_at'])
+    HaravanOrderInfo.find_or_create_by(haravan_order_id: record['id'],
+                                       total_price: record['total_price'],
+                                       ordered_at: record['created_at'])
   end
 end
---
+++ db/seeds/haravan_product_dump.rb
@@ -0,0 +1,8 @@
+extend Haravan::ApiService::HaravanProduct
+extend Haravan::ApiService
+
+ApplicationRecord.transaction do
+  records = self.save_haravan_products('2020-08-13T07:00:00.000Z', '2020-08-20T06:59:59.999Z')
+
+  ap records.first
+end
