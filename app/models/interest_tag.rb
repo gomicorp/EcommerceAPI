591900222ed6700256601fb14b8c948abf6e5106
Bran <40688044+june20516@users.noreply.github.com>
2020-09-16 14:55:31 +0900

    버그 : 마이그레이트 태스크가 깨지는 문제 해결

--- app/models/interest_tag.rb
+++ app/models/interest_tag.rb
@@ -3,19 +3,9 @@
 # Table name: interest_tags
 #
 #  id         :bigint           not null, primary key
-#  created_by :string(255)
 #  name       :json
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  country_id :bigint
-#
-# Indexes
-#
-#  index_interest_tags_on_country_id  (country_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (country_id => countries.id)
 #
 class InterestTag < ApplicationRecord
 end
