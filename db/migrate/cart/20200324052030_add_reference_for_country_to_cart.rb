627a17e33694da1c41380d12714dda580680dd9d
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2020-09-24 11:54:04 +0900

    수정: 장바구니에 컨트리 모델 연결을 해당 스키마가 없는경우에만 추가하도록 수정

--- db/migrate/cart/20200324052030_add_reference_for_country_to_cart.rb
+++ db/migrate/cart/20200324052030_add_reference_for_country_to_cart.rb
@@ -1,5 +1,5 @@
 class AddReferenceForCountryToCart < ActiveRecord::Migration[6.0]
   def change
-    add_reference :carts, :country, foreign_key: true
+    add_reference(:carts, :country, foreign_key: true) unless Cart.attribute_method?(:country)
   end
 end
