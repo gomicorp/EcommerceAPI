9652b3b6d7a1393e93ea346929fc77de257e0ded
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2020-09-24 12:10:32 +0900

    수정: 어노테이션 주석 추가

--- app/models/adjustment_product_item.rb
+++ app/models/adjustment_product_item.rb
@@ -13,6 +13,11 @@
 #  index_adjustment_product_items_on_adjustment_id    (adjustment_id)
 #  index_adjustment_product_items_on_product_item_id  (product_item_id)
 #
+# Foreign Keys
+#
+#  fk_rails_...  (adjustment_id => adjustments.id)
+#  fk_rails_...  (product_item_id => product_items.id)
+#
 class AdjustmentProductItem < ApplicationRecord
   belongs_to :adjustment
   belongs_to :product_item
--- app/models/sellers/item_sold_paper.rb
+++ app/models/sellers/item_sold_paper.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: sellers_item_sold_papers
+#
+#  id              :bigint           not null, primary key
+#  adjusted_profit :integer          default(0)
+#  paid            :boolean          default(FALSE)
+#  paid_at         :datetime
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  item_id         :bigint           not null
+#  seller_info_id  :bigint           not null
+#
+# Indexes
+#
+#  index_sellers_item_sold_papers_on_item_id         (item_id)
+#  index_sellers_item_sold_papers_on_seller_info_id  (seller_info_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (item_id => cart_items.id)
+#  fk_rails_...  (seller_info_id => sellers_seller_infos.id)
+#
 module Sellers
   class ItemSoldPaper < ApplicationRecord
     belongs_to :item, class_name: 'CartItem'
