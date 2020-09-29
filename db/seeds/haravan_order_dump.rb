d72c5a5d99016e021d2f03743b66fb4f1a9ba2ce
JW <jjwoo93@naver.com>
2020-09-29 15:06:52 +0900

    수정: 하라반 주문 정보 덤프 로직 수정

--- db/seeds/haravan_order_dump.rb
+++ db/seeds/haravan_order_dump.rb
@@ -5,8 +5,18 @@ ApplicationRecord.transaction do
   records = self.get_order_by_period('2020-08-13T07:00:00.000Z', '2020-08-20T06:59:59.999Z')
 
   records.each do |record|
-    HaravanOrderInfo.find_or_create_by(haravan_order_id: record['id'],
-                                       total_price: record['total_price'],
-                                       ordered_at: record['created_at'])
+    ship_fee = 0
+    record['shipping_lines'].each do |line|
+      ship_fee += line['price']
+    end
+
+    order_info = HaravanOrderInfo.find_or_create_by(haravan_order_id: record['id'])
+    order_info.update(total_price: record['total_price'],
+                      ordered_at: record['created_at'],
+                      pay_method: record['gateway'],
+                      channel: record['source'],
+                      paid_at: record['gateway'] == "Thanh toán khi giao hàng (COD)" ? record['fulfillments'][0]['cod_paid_date'] : record['created_at'],
+                      order_status: record['financial_status'],
+                      ship_fee: ship_fee)
   end
 end
