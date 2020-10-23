4035bf9e16fbf35f629fcd222541f8dc96daade2
JW <jjwoo93@naver.com>
2020-10-23 10:46:57 +0900

    수정: 어댑터 추상클래스 메소드 위치 수정

--- app/adapters/external_channel_adapter.rb
+++ app/adapters/external_channel_adapter.rb
@@ -2,20 +2,18 @@ class ExternalChannelAdapter
 
   def initialize; end
 
-  protected
-  def login; end
-
   public
   # == 적절하게 정제된 데이터를 리턴합니다.
   def products(query = {}); end
   def orders(query = {}); end
 
   protected
+  def login; end
+
   # == 외부 채널의 API 를 사용하여 각 레코드를 가져옵니다.
   def call_products(query); end
   def call_orders(query); end
 
-  protected
   # == call_XXX 로 가져온 레코드를 정제합니다.
   def refine_products(records); end
   def refine_orders(records); end
