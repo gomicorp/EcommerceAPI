0e6e4ad5680f573dd3629f3cea844207114564fb
JW <jjwoo93@naver.com>
2020-10-26 14:11:43 +0900

    수정: migrate를 진행함에 따른 annotation 추가

--- app/models/external_channel_product_id.rb
+++ app/models/external_channel_product_id.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: external_channel_product_ids
+#
+#  id          :bigint           not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  channel_id  :bigint
+#  external_id :string(255)
+#  product_id  :bigint
+#
+# Indexes
+#
+#  index_external_channel_product_ids_on_channel_id  (channel_id)
+#  index_external_channel_product_ids_on_product_id  (product_id)
+#
 class ExternalChannelProductId < ApplicationRecord
   belongs_to :product, dependent: :destroy
   belongs_to :channel, dependent: :destroy
