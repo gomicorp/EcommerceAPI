aab03e6c2ee41baf01114cb3df4c6ef066711b84
Lee seung chan <dltmdcks702@gmail.com>
2020-10-26 17:57:42 +0900

    리펙토링: sendo adapter의 call product 구조 분리
    상품 하나하나에 대해서 모두 개별 검색을 해야 option들이 나온다. 따라서 해당 요청의 로직이 길면 보기 불편하여 분리했다.

--- app/adapters/external_channel/sendo_adapter.rb
+++ app/adapters/external_channel/sendo_adapter.rb
@@ -10,6 +10,7 @@ module ExternalChannel
     end
 
     public
+
     # == 적절하게 정제된 데이터를 리턴합니다.
     def products(query_hash = {})
       refine_products(call_products(query_hash))
@@ -20,7 +21,7 @@ module ExternalChannel
     end
 
     protected
-    
+
     def login
       api_key = Rails.application.credentials.dig(:sendo, :api,  :key)
       api_password = Rails.application.credentials.dig(:sendo, :api, :password)
@@ -36,25 +37,9 @@ module ExternalChannel
     # == 외부 채널의 API 를 사용하여 각 레코드를 가져옵니다.
     def call_products(query_hash = {})
       products_url = URI(url_with_query_hash("#{base_url}/api/partner/product/search", query_hash))
-      next_token = ""
-      products = []
-      while next_token.nil? == false
-        product = call_product(products_url, next_token)
-        next_token = product["result"]["next_token"]
-        ap next_token
-        products.append(product["result"]["data"])
+      call_all_post(products_url) do |data|
+        data["result"]["data"].pluck("id")
       end
-      products.flatten
-    end
-
-    def call_product(url, next_token)
-      products_body = {token: next_token, page_size: 50}
-      products_header = {
-        'Content-Type': 'application/json',
-        'Authorization': "Bearer #{@@token}"
-      }
-      response = req_post_json(url, products_body, products_header)
-      JSON.parse response.body
     end
 
     def call_orders(query); end
@@ -63,7 +48,7 @@ module ExternalChannel
     def refine_products(records); end
     def refine_orders(records); end
 
-    # == json으로 포스트를 날립니다.
+    # == json으로 Post요청을 날립니다.
     def req_post_json(url, body={}, headers={})
       request = Net::HTTP::Post.new(url)
       request.body = body.to_json
@@ -76,6 +61,55 @@ module ExternalChannel
         o.use_ssl = true
       end
       http.request(request)
+    end
+
+    # == json으로 Get요청을 날립니다.
+    def req_get_json(url, body={}, headers={})
+      request = Net::HTTP::Get.new(url)
+      request.body = body.to_json
+
+      headers.each_pair do |key, value|
+        request[key] = value
+      end
+
+      http = Net::HTTP.new(url.host, url.port).tap do |o|
+        o.use_ssl = true
+      end
+      http.request(request)
+    end
+
+    private
+
+    # == list 에서 모든 데이터를 요청합니다.
+    def call_all_post(url)
+      next_token = ""
+      data = []
+      while next_token.nil? == false
+        each_data = call_each(url, next_token)
+        next_token = each_data["result"]["next_token"]
+        ap next_token
+        data << if block_given?
+                  yield each_data
+                else
+                  each_data
+                end
+      end
+      data.flatten
+    end
+
+    # == 하나의 post요청 혹은 원하는 요청을 보냅니다.
+    def call_each(url, next_token)
+      body = {token: next_token, page_size: 50}
+      header = {
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer #{@@token}"
+      }
+      response = if block_given?
+                   yield (url, body, header)
+                 else
+                   req_post_json(url, body, header)
+                 end
+      JSON.parse response.body
     end
   end
 end
