e048a3c248f2caa5f9e9d78f3a50c640aad27829
Lee seung chan <dltmdcks702@gmail.com>
2020-11-03 16:31:29 +0900

    리펙토링: 들어온 데이터가 nil일 경우 데이터를 기존 데이터로 입력
    # 의도
    ### 변경하게된 배경 맥락
    - order의 shipping status, cancel status, paid_at은 nullable하다.
        - shopee와 sendo 모두 order status를 이용해서 shipping status와 cancel status를 받는다. sendo는 paid at이 없고, 쇼피는 nullable하다. 이로 인해 기존 데이터는 nil이 아닌데, nil값이 되는 경우가 있을 수 있다. 이와 같은 상황을 방지하고자 변경했다.
        - 세부적인 배경 설명
    
        shopee는 shipping 이 완료 후 입금이 완료되면 order status가 complete가 된다. shipping status는 order status로 부터 만들어지기 때문에 ship과 관련되지 않아 nil로서 데이터가 입력될 것이다. 이때, shipping status는 shipped로서 남아있어야 한다. 이를 위해서 기존의 데이터가 존재할 경우 nil인 데이터에 대해서 덮어 쓰는 방식으로 변경했다.
    
    # TODO
    위와 같은 변경에도 불구하고, 경우에 따라 과정이 생략된 order등이 들어올 경우 예외가 생긴다. 이와 같은 예외를 다루기 위해 아래 과정이 필요하다.
    1. 생각할 수 있는 예외적인 상황을 브레인 스토밍을 통해서 도출, 정리한다.
    2. shopee 등 외부 채널에서 들어오는 실제 데이터를 바탕으로 1에서 도출한 상황에 대해서 어떻게 대처를 할지 계획을 세운다.
    3. 계획에 따라서 작업과 기한을 생성하고, 작업을 진행한다.

--- app/services/external_channel/order/saver.rb
+++ app/services/external_channel/order/saver.rb
@@ -9,7 +9,7 @@ module ExternalChannel
       def save(order)
         order_info = ::ExternalChannelOrderInfo.find_by(external_channel_order_id: order[:id])
         order_info = ::ExternalChannelOrderInfo.new if order_info.nil?
-        order_info.update!(parse_order(order))
+        order_info.update!(parse_order(order, order_info.attributes))
         set_order_related_info(order_info, order[:variant_ids]) if order_info.products.nil?
       end
 
@@ -36,20 +36,20 @@ module ExternalChannel
         end
       end
 
-      def parse_order(order)
+      def parse_order(order, default_order={})
         # TODO 모델 구조 변경시 변경 필요
         {
           total_price: order[:billing_amount],
           ordered_at: order[:ordered_at],
           pay_method: order[:pay_method],
           channel: order[:channel],
-          paid_at: order[:paid_at],
+          paid_at: order[:paid_at] || default_order[:paid_at],
           order_status: order[:order_status],
           ship_fee: order[:ship_fee],
           order_number: order[:order_number],
-          cancelled_status: order[:cancelled_status],
+          cancelled_status: order[:cancelled_status] || default_order[:shipping_status],
           external_channel_order_id: order[:id],
-          shipping_status: order[:shipping_status]
+          shipping_status: order[:shipping_status] || default_order[:shipping_status]
         }
       end
 
