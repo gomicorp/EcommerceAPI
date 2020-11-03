8f3b74aea846016ca409089e2ef89d1ac63c069f
Lee seung chan <dltmdcks702@gmail.com>
2020-11-03 12:02:08 +0900

    기능: order데이터를 shopee로 부터 가져오기
    # order 데이터를 shopee로 부터 가져오기
    ## 검색
    list를 받아서 50개씩 따로 검색함.
    
    ## retain
    가져온거 다 retain
    ### TODO
    cancel status와 shipping status는 order status에 의해 결정되는데, 이때 null을 반환하게 했다. 그럴 경우, 저장시에 들어오는 데이터가 null일때는 데이터 치환을 하지 않고, 실제로 있을때만 데이터 치환을 하도록 하는 처리가 필요함.

--- app/adapters/external_channel/shopee_adapter.rb
+++ app/adapters/external_channel/shopee_adapter.rb
@@ -7,14 +7,19 @@ module ExternalChannel
     #{
     #  update_time_from
     #  update_time_to
-    #  pagination_offset : not null
-    #  pagination_entries_per_page : not null, max 100
+    #  pagination_offset : default 0
+    #  pagination_entries_per_page : default 10, max 100
     #}
 
 
     # === 사용 가능한 Order query property (공식 API 문서 기준이고, 변경될 가능성이 있습니다)
     # https://open.tiki.vn/#order-api-v2
     #{
+    #  order_status: ALL/UNPAID/READY_TO_SHIP/COMPLETED/IN_CANCEL/CANCELLED/TO_RETURN, default: ALL
+    #  create_time_from
+    #  create_time_to
+    #  pagination_entries_per_page: default 0
+    #  pagination_offset: default 100, max 100
     #}
 
     def initialize
@@ -41,8 +46,7 @@ module ExternalChannel
     # == 외부 채널의 API 를 사용하여 각 레코드를 가져옵니다.
     def call_products(query_hash = {})
       query_hash[:pagination_offset] ||= 0
-      # === TODO: 테스트를 위해서 개수를 10개로 제한.
-      query_hash[:pagination_entries_per_page] ||= 10
+      query_hash[:pagination_entries_per_page] ||= 100
 
       products_url = "#{base_url}/items/get"
       products_body = set_shopee_body(query_hash)
@@ -54,7 +58,19 @@ module ExternalChannel
       call_product_by_ids(product_ids.flatten)
     end
 
-    def call_orders(query_hash)
+    def call_orders(query_hash = {})
+      query_hash[:pagination_offset] ||= 0
+      query_hash[:pagination_entries_per_page] ||= 100
+      query_hash[:order_status] ||= 'ALL'
+
+      orders_url = "#{base_url}/orders/get"
+      orders_body = set_shopee_body(query_hash)
+
+      orders_sn = []
+      call_list(orders_url, orders_body) do |response|
+        orders_sn<< response['orders'].pluck('ordersn')
+      end
+      call_order_by_sn(orders_sn.flatten)
     end
     # == call_XXX 로 가져온 레코드를 정제합니다.
     def refine_products(products)
@@ -69,41 +85,49 @@ module ExternalChannel
       end
     end
 
-    # === TODO: No Brand인 경우 베트남 측에서 관리하지 않은 브랜드로 보인다. 이것을 어떻게 처리할지 논의가 필요하다.
-    # === 우려되는 사항은 다음과 같다.
-    # === 1. 가격등 브랜드와 관련 없는 정보가 바뀔 경우 브랜드를 다시 No Brand로 설정 할 수 있다.
-    def refine_brand_name(attr)
-      brand_attr = attr.find{|attr| attr['attribute_name'] == "Thương hiệu"}
-      brand_attr['attribute_value']
-    end
-
-    def refine_variants(product)
-      variants = product['variations']
-
-      # === variants가 없으면 product를 참조 해야 한다.
-      return [{
-          id: product['item_id'],
-          price: product['price'],
-          name: 'default'
-      }] if variants.empty?
-
-      variants.map do |variant|
-        {id: variant['variation_id'], price: variant['price'], name: variant['name']}
+    def refine_orders(orders)
+      orders.map do |order|
+        {
+            id: order['ordersn'],
+            order_number: order['ordersn'],
+            order_status: order['order_status'],
+            pay_method: order['payment_method'],
+            channel: 'shopee',
+            ordered_at: Time.new(order['create_time']).getutc,
+            paid_at: paid_time(order['pay_time']),
+            billing_amount: order['escrow_amount'],
+            ship_fee: order['actual_shipping_cost'],
+            variants_ids: variants(order['items']),
+            canceled_status: cancel_status(order['order_status']),
+            shipping_status: shipping_status(order['order_status'])
+        }
       end
-    end
-
-    def refine_orders(records)
     end
 
     private
 
     attr_reader :partner_id, :key, :shop_id
 
+    ### === Shoppe에서 데이터를 가져오는 부분
+
     def call_product_by_ids(ids)
       url = "#{base_url}/item/get"
       call_each_by_ids(ids, url, 'item') do |id|
         {item_id: id}
       end
+    end
+
+    def call_order_by_sn(sn)
+      orders = []
+      url = "#{base_url}/orders/detail"
+      # === shopee 에서 받을 수 있는 order의 개수를 50개로 제한함.
+      sn.each_slice(50).map do |each_sn|
+        body = set_shopee_body({ordersn_list: each_sn})
+        header = get_shopee_header(url, body)
+        response = request_post(url, body, header)
+        orders << response['orders']
+      end
+      orders.flatten
     end
 
     # body를 의미하는 block을 받아, 모든 id에 대해 post요청을 보내고 타겟에 대한 묶음을 전달하는 함수.
@@ -126,8 +150,7 @@ module ExternalChannel
     def call_list(url, body)
       more = true
       body[:pagination_offset] ||= 0
-      # ===  Todo: 테스트를 위해 pagination_offset을 1개로 고정
-      while more && body[:pagination_offset] < 1 do
+      while more do
         header = get_shopee_header(url, body)
         response = request_post(url, body, header)
         more = response['more']
@@ -171,6 +194,53 @@ module ExternalChannel
     def make_shopee_signature(url, body)
       signature_base = url + '|' + body.to_json
       OpenSSL::HMAC.hexdigest('sha256', key, signature_base)
+    end
+
+    ### === Shopee 에서 가져온 데이터를 정제하는 부분
+
+    # === product data refine
+
+    # === TODO: No Brand인 경우 베트남 측에서 관리하지 않은 브랜드로 보인다. 이것을 어떻게 처리할지 논의가 필요하다.
+    # === 우려되는 사항은 다음과 같다.
+    # === 1. 가격등 브랜드와 관련 없는 정보가 바뀔 경우 브랜드를 다시 No Brand로 설정 할 수 있다.
+    def refine_brand_name(attr)
+      brand_attr = attr.find{|attr| attr['attribute_name'] == "Thương hiệu"}
+      brand_attr['attribute_value']
+    end
+
+    def refine_variants(product)
+      variants = product['variations']
+
+      # === variants가 없으면 product를 참조 해야 한다.
+      return [{
+                  id: product['item_id'],
+                  price: product['price'],
+                  name: 'default'
+              }] if variants.empty?
+
+      variants.map do |variant|
+        {id: variant['variation_id'], price: variant['price'], name: variant['name']}
+      end
+    end
+
+    #=== order 데이터 refine
+
+    def paid_time(pay_time)
+      pay_time ? Time.new(pay_time).getutc : nil
+    end
+
+    def cancel_status(order_status)
+      order_status == 'CANCEL' ? order_status : nil
+    end
+
+    def shipping_status(order_status)
+      %w(READY_TO_SHIP RETRY_SHIP SHIPPED).include? order_status ? order_status : nil
+    end
+
+    def variants(variants)
+      variants.map do |variant|
+        [variant['variation_id'] || variant['item_id'], variant['variation_quantity_purchased']]
+      end
     end
   end
 end
