35033044388bd01922c626fb7dcc8f789e224023
Lee seung chan <dltmdcks702@gmail.com>
2020-11-04 11:17:19 +0900

    기능: sendo adapter 및 base adapter 추가 (#95)
    * 기능: sendo 로그인 및 상품 가져오기 기능 추가
    
    * 리펙토링: sendo adapter의 call product 구조 분리
    
    상품 하나하나에 대해서 모두 개별 검색을 해야 option들이 나온다. 따라서 해당 요청의 로직이 길면 보기 불편하여 분리했다.
    
    * 기능: 센도의 product를 filtering에 따라서 전부 불러오도록 처리
    
    한번의 요청으로 모든 상품에 대한 조회가 시작됨.
    call_products를 통해서 상품 상세 정보 array가 리턴 됨.
    
    * 기능: sendo adapter에서 데이터 정제하는 로직 추가
    
    * 기능: 센도의 오더정보 받아오기 기능 구현 및 옵션 없는 경우 대응
    
    - 오더 정보를 받아오는 call_orders를 구현
    - 상품 정보 중 variants 항목이 없는 경우가 발견되었다. 이에 따라 default option을 형성하는 방식으로 데이터를 형성하도록 조치하였다.
    
    * 기능: 센도 오더 데이터를 변환함
    
    - 센도 오더 데이터를 사전에 정의한 데이터 형식으로 변환함.
    
    * 버그: 어뎁터 명 변경에 따른 변경
    
    * 버그: 어뎁터 명 변경에 따른 변경
    
    > app/adapters/external_channel/sendo_adapter
    부모클래스의 이름 변경에 따른 수정
    
    > app/adapters/external_channel/base_adatper
    - 함수 설명
    url과 query hash를 입력받고, url뒤에 get 요청을 위한 query를 붙여줍니다.
    - 함수 선언 이유
    1. 코드 한줄이지만 의도를 명확하게 사용할 수 있다.
    2. get요청을 위해 어떻게 url을 만들어야 하는지에 관해 잠깐의 시간동안 고민하지 않아도 된다.
    - base adatper에 추가한 이유
    1. 간단한 함수라서 모든 adapter의 get요청에 쓰일 수 있다.
    
    * 기능: Get요청 url 만들어 주는 함수 추가
    
    ### url_with_query_hash(url, query_hash) 함수를 추가
    > app/adapters/external_channel/base_adapter
    - 추가한 이유
    1.  읽는 사람이 코드를 읽으면서 함수명만으로 특별한 사고 없이 해당 부분의 의미를 알 수 있다.
    2. 추후 adapter를 추가할 때, get 요청을 위해 url을 어떻게 만들지 고민하는 그 찰나의 집중 방해 요소를 방지할 수 있다.
    - base adapter에 추가한 이유
    1. 모든 get 요청에 사용할 수 있는 순수함수이기 때문이다.
    
    * 리펙토링: sendo adapter를 Faraday로 리펙토링
    
    # Faraday리펙토링
    | app/adapters/external_channel/sendo_adapter
    - 기존에 NET::HTTP 모듈을 사용하던 것을 Faraday gem으로 교환
        - 코드가 길어져 맥락을 파악하기 어려워져 Faraday gem을 사용하도록 교체했다.
    # url_with_query_hash 삭제
    | app/adapters/external_channel/base_adapter
    - 기존에 Faraday를 사용하지 않아서 get의 query를 만들어 줄 필요가 있었다. faraday로 교체하면서 위 함수는 자연스럽게 필요 없게 되어 삭제했다.
    
    * 기능: sendo credential 추가

---
+++ app/adapters/external_channel/sendo_adapter.rb
@@ -0,0 +1,249 @@
+module ExternalChannel
+  class SendoAdapter < BaseAdapter
+    # Product 요청 파라미터
+    # {
+    #   date_form: yyyy/mm/dd string,
+    #   date_to: yyyy/mm/dd string,
+    #   product_name: string,
+    # }
+    #
+    # Order 요청 파라미터
+    # {
+    #   order_date_from: yyyy/mm/dd string
+    #   order_date_to: yyyy/mm/dd string
+    #   order_status_date_from: yyyy/mm/dd string (주문 변경 일자)
+    #   order_status_date_to: yyyy/mm/dd string (주문 변경 일자)
+    #   order_status: 주문 상태
+    # }
+
+    attr_reader :base_url
+
+    def initialize
+      @base_url = "https://open.sendo.vn"
+      login
+    end
+
+    protected
+
+    # == 적절하게 정제된 데이터를 리턴합니다.
+    @override
+    def products(query_hash = {})
+      refine_products(call_products(query_hash))
+    end
+
+    @override
+    def orders(query_hash = {})
+      refine_orders(call_orders(query_hash))
+    end
+
+    @override
+    def login
+      api_key = Rails.application.credentials.dig(:sendo, :api,  :key)
+      api_password = Rails.application.credentials.dig(:sendo, :api, :password)
+      login_url = URI("#{base_url}/login")
+      login_body = {shop_key: api_key, secret_key: api_password}
+      login_header = {'Content-Type': 'application/json'}
+      response = Faraday.post(login_url, login_body.to_json, login_header)
+      data = JSON.parse response.body
+      # 새로 발급받은 토큰만 유효하므로 기존 토큰을 대체함.
+      @@token = data["result"]["token"]
+    end
+
+    # == 외부 채널의 API 를 사용하여 각 레코드를 가져옵니다.
+    @override
+    def call_products(query_hash = {})
+      products_url = URI("#{base_url}/api/partner/product/search")
+      products_body = {
+        token: '',
+        page_size: 50
+      }.merge(query_hash)
+      products_header = {
+        'Authorization': "bearer #{@@token}",
+        'Content-Type': 'application/json',
+      }
+      product_ids = request_lists(products_url, products_body, products_header) do |products|
+        products["result"]["data"].pluck("id")
+      end
+      get_all_by_ids(product_ids, "#{base_url}/api/partner/product")
+    end
+
+    @override
+    def call_orders(query_hash = {})
+      orders_url = URI("#{base_url}/api/partner/salesorder/search")
+      orders_body = {
+        token: ''
+      }.merge(query_hash)
+      orders_header = {
+        'Authorization': "bearer #{@@token}",
+        'Content-Type': 'application/json',
+        'cache-control': 'no-cache'
+      }
+      request_lists(orders_url, orders_body, orders_header) do |orders|
+        orders["result"]["data"]
+      end
+    end
+
+    # == call_XXX 로 가져온 레코드를 정제합니다.
+    @override
+    def refine_products(products)
+      products.map do |product|
+        {
+          id: product["id"],
+          title: product["name"],
+          channel_name: 'sendo',
+          brand_name: product["brand_name"] || 'not brand',
+          variants: form_variants(product)
+        }
+      end
+    end
+
+    @override
+    def refine_orders(orders)
+      orders.map do |order|
+        sales_data = order["sales_order"]
+        sales_details = order["sku_details"]
+        {
+          id: sales_data["order_number"],
+          order_number: sales_data["order_number"],
+          billing_amount: sales_data["total_amount_buyer"],
+          variants_ids: sales_details.map {|option| [option["product_variant_id"], option["quantity"]]},
+          order_status: map_order_status(sales_data["order_status"]),
+          cancel_status: cancelled_status(sales_data["order_status"]),
+          shipping_status: shipping_status(sales_data["order_status"]),
+          pay_method: map_pay_method(sales_data["payment_method"])
+        }
+      end
+    end
+
+    private
+
+    ### === 데이터를 불러오는 로직입니다.
+
+    def get_all_by_ids(ids, url, query_hash = {})
+      header = {
+          'Content-Type': 'application/json',
+          'Authorization': "Bearer #{@@token}"
+      }
+
+      ids.map do |id|
+        query_hash[:id] = id
+        record = JSON.parse(Faraday.get(url, query_hash, header).body)
+        if block_given?
+          yield record
+        else
+          record["result"]
+        end
+      end
+    end
+
+    # == list 에서 모든 데이터를 요청합니다.
+    def request_lists(url, body, header)
+      body[:token] = ""
+      data = []
+      while body[:token].nil? == false
+        response = Faraday.post(url, body.to_json, header)
+        each_data = JSON.parse response.body
+        body[:token] = each_data["result"]["next_token"]
+        data << if block_given?
+                  yield each_data
+                else
+                  each_data
+                end
+      end
+      data.flatten
+    end
+
+
+    ### === 데이터를 정제하는 로직입니다.
+
+
+    ## === product 데이터를 정제합니다.
+
+    def form_variants(product)
+      variants = product["variants"]
+
+      # variants 가 빈배열인 데이터가 있다.
+      # 이 경우, default 옵션 데이터를 만들어 줄 필요가 있다.
+      return [
+        {
+          id: product["id"],
+          name: 'default',
+          price: product["price"]
+        }
+      ] if variants.empty?
+
+      variants.map do |variant|
+        {
+          id: variant["variant_sku"],
+          name: variant["variant_sku"],
+          price: variant["variant_price"]
+        }
+      end
+    end
+
+    ## === order 데이를 정제합니다.
+
+    def cancelled_status(order_status)
+      if order_status == 13
+        map_order_status(order_status)
+      else
+        nil
+      end
+    end
+
+    def shipping_status(order_status)
+      if [6,7,8].include? order_status
+        map_order_status(order_status)
+      else
+        nil
+      end
+    end
+
+    # == 센도의 결제 방법(숫자) 글자로 바꿔주는 함수입니다.
+    # == TODO: 주문 상태 리펙토링 후 아래 로직을 수정해야 합니다.
+    # == 우리의 판매 방법의 수가 센도의 판매 방법의 수 보다 같거나 많을 것이라고 판단하여 아래와 같이 case 구문으로 구현하였습니다.
+    def map_pay_method(sendo_pay_method)
+      case sendo_pay_method
+      when 1
+        'COD'
+      when 2
+        'Senpay'
+      when 4
+        'Combine'
+      when 5
+        'PayLater'
+      else
+        nil
+      end
+    end
+
+    # == 센도의 주문상태(숫)를 글자로 바꿔주는 함수입니다.
+    # == TODO: 주문 상태 리펙토링 후 아래 로직을 수정해야 합니다.
+    # == 우리의 주문 상태가 센도의 주문 상태보다 수가 적을 것이라고 판단하여 아래와 같이 array로 구현하였습니다.
+    def map_order_status(sendo_order_status)
+      allowed_order_status = {
+        'New': [2],
+        'Proccessing': [3],
+        'Shipping': [6],
+        'POD': [7],
+        'Completed': [8],
+        'Closed': [10],
+        'Delaying': [11],
+        'Delay': [12],
+        'Cancelled': [13],
+        'Splitting': [14],
+        'Splitted': [15],
+        'Merging': [19],
+        'Returning': [21],
+        'Returned': [22],
+        'WaitingSendo': [23]
+      }
+      order_status = allowed_order_status.select {|key, value| value.include?(sendo_order_status)}
+      if order_status.empty?
+        nil
+      else
+        order_status.keys[0].to_s
+      end
+    end
+  end
+end
