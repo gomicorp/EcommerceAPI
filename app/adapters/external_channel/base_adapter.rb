822b557592d66fb08f314c972a42e6d7ef408acb
JW <jjwoo93@naver.com>
2020-11-05 16:58:42 +0900

    수정: base_adapter에 토큰 검증 메소드 추가

--- app/adapters/external_channel/base_adapter.rb
+++ app/adapters/external_channel/base_adapter.rb
@@ -19,10 +19,14 @@ module ExternalChannel
 
     protected
 
+    # == 토큰이 필요하며, 유효성을 검사해야하는 어댑터
+    def check_token_validation; end
+
     # == 적절하게 정제된 데이터를 리턴합니다.
     def products(query = {}); end
     def orders(query = {}); end
 
+    # == 로그인이 필요한 어댑터
     def login; end
 
     # == 외부 채널의 API 를 사용하여 각 레코드를 가져옵니다.
