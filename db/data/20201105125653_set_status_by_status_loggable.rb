d1717f99a7d3665477b2cce31cd8063c75e9648d
Bran <40688044+june20516@users.noreply.github.com>
2020-11-10 12:42:51 +0900

    기능 : STORE-132 / 주문상태 변경 데이터 마이그레이션 예외 처리 및 간단한 로그 출력

--- db/data/20201105125653_set_status_by_status_loggable.rb
+++ db/data/20201105125653_set_status_by_status_loggable.rb
@@ -48,7 +48,7 @@ class SetStatusByStatusLoggable < ActiveRecord::Migration[6.0]
       Cart: 'ordered',
       ShipInfo: nil,
       # 결제 대기 상태일 때 취소 했을 수도 있고, 결제 완료 후 최소했을 수도 있다.
-      Payment: {not_paid: 'pay_wait', paid: 'paid'},
+      Payment: { not_paid: 'pay_wait', paid: 'paid' },
       OrderInfo: 'cancel_request'
     },
     # 없음
@@ -69,7 +69,7 @@ class SetStatusByStatusLoggable < ActiveRecord::Migration[6.0]
       Cart: 'archived',
       ShipInfo: nil,
       # 결제 대기 상태일 때 취소 했을 수도 있고, 결제 완료 후 최소했을 수도 있다.
-      Payment: {not_paid: 'pay_wait', paid: 'refund_complete'},
+      Payment: { not_paid: 'pay_wait', paid: 'refund_complete' },
       OrderInfo: 'cancel_complete'
     },
     # 없음
@@ -109,68 +109,76 @@ class SetStatusByStatusLoggable < ActiveRecord::Migration[6.0]
       setup_order_component_statuses
 
       # 특정 상태의 cart들을 뽑아옵니다.
-      STATUS_MAP.each do |cart_status, order_components_stat|
+      STATUS_MAP.each do |cart_status, order_comps_stat|
         carts = Cart.where(order_status: cart_status)
         ap "Carts in #{cart_status} will be updated. (count : #{carts.count})"
-        # hand면 패스합니다.
-        next if cart_status == :hand
-        # 없으면 패스합니다.
-        next if carts.empty?
+        # hand면 패스합니다. 카트가 없으면 패스합니다.
+        next if cart_status == :hand || carts.empty?
 
-        # cancel 케이스는 결제 현황을 체크해서 반영해야 합니다.
-        if %w[cancel-request cancel-complete].include? cart_status
-          carts.each do |c|
-            update_order_comps_status_with_payment_clue(order_components_stat, c.order_info)
+        carts.each do |c|
+          order = c.order_info
+          # order가 고의로 지워진 cart가 몇건 있습니다. 수동입력 주문들입니다.
+          if order.nil?
+            c.update_status(order_comps_stat[:Cart])
+            next
           end
-        else
-          carts.each do |c|
-            update_order_comps_by_stat_map(order_components_stat, c.order_info)
+
+          # cancel 케이스는 결제 현황을 체크해서 반영해야 합니다.
+          if %i[cancel-request cancel-complete].include? cart_status
+            update_order_comps_status_with_payment_clue(order_comps_stat, order)
+          else
+            update_order_comps_by_stat_map(order_comps_stat, order)
           end
         end
       end
     end
   end
+end
 
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
+def down
+  raise ActiveRecord::IrreversibleMigration
+end
 
-  private
+private
 
-  def setup_order_component_statuses
-    order_components = [Cart, Payment, ShipInfo]
+def setup_order_component_statuses
+  order_components = [Cart, Payment, ShipInfo]
 
-    order_components.each do |component|
-      component.available_status.map do |status|
-        Country.all.each do |country|
-          component::StatusCode.find_or_create_by(name: status, country: country)
-        end
+  order_components.each do |component|
+    component.available_status.map do |status|
+      Country.all.each do |country|
+        component::StatusCode.find_or_create_by(name: status, country: country)
       end
-      ap "#{component.name} status codes are created."
     end
+    ap "#{component.name} status codes are created."
+  end
+end
+
+def update_order_comps_status_with_payment_clue(order_comps_stat, order)
+  # component 별개로 업데이트
+  %i[Cart ShipInfo].each do |comp_class|
+    status = order_comps_stat[comp_class]
+    component = order.send(comp_class.to_s.underscore.to_sym)
+    component.update_status(status) unless status.nil?
+    ap "======== #{component.class.name} | id : #{component.id} | status : #{status} ========"
   end
 
-  def update_order_comps_status_with_payment_clue(order_components_stat, order)
-    # component 별개로 업데이트
-    %i[Cart ShipInfo].each do |comp|
-      status = order_components_stat[comp]
-      order.send(comp.to_s.underscore.to_sym).update_status(status) unless status.nil?
-    end
+  payment = order.payment
+  clue = payment.paid? ? :paid : :not_paid
+  status = order_comps_stat.dig(:Payment, clue)
+  order.payment.update_status(status)
+  ap "======== Payment | id : #{payment.id} | status : #{status} ========"
 
-    clue = order.payment.paid? ? :paid : :not_paid
-    order.payment.update_status(order_components_stat.dig(:Payment, clue))
+  order.update_status order_comps_stat[:OrderInfo]
+  ap "======== OrderInfo | id : #{order.id} | status : #{order_comps_stat[:OrderInfo]} ========"
+end
 
-    order.update_status order_components_stat[:OrderInfo]
-  end
-
-  def update_order_comps_by_stat_map(order_components_stat, order)
-    # 해당 상태의 component들의 status를 체크합니다.
-    order_components_stat.each do |model_name, status|
-      # 해당 상태의 order와 연결된 component의 status를 업데이트 합니다.
-      component = model_name == :OrderInfo ? order : order.send(model_name.to_s.underscore.to_sym)
-      ap "======== #{component.class.name} : #{component.id}========"
-      ap "======== #{status} ========"
-      component.update_status(status) unless status.nil?
-    end
+def update_order_comps_by_stat_map(order_comps_stat, order)
+  # 해당 상태의 component들의 status를 체크합니다.
+  order_comps_stat.each do |model_name, status|
+    # 해당 상태의 order와 연결된 component의 status를 업데이트 합니다.
+    component = model_name == :OrderInfo ? order : order.send(model_name.to_s.underscore.to_sym)
+    component.update_status(status) unless status.nil?
+    ap "======== #{component.class.name} | id : #{component.id} | status : #{status} ========"
   end
 end
