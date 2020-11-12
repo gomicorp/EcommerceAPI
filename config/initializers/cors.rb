bce512e69d869108438f3bdb3f2bd863bb50be23
JW <jjwoo93@naver.com>
2020-11-12 16:26:07 +0900

    수정: cors에서 허용하는 origin의 폭을 줄임

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins(
+      'sellers.gomistore.in.th',  # 디딤돌용 셀러즈 모바일앱
+      'sellers.gomistore.com',    # 디딤돌용 고미스토어 (+팝업스토어)
+      'partners.gomistore.com',   # 디딤돌용 파트너센터
+      'partner.gomistore.com'
+    )
     resource(
       '*',
       headers: :any,
