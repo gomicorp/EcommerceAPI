afc85fc376823cd58841a80d4695461400a65014
JW <jjwoo93@naver.com>
2020-11-13 17:18:46 +0900

    수정: 외부채널 컨트롤러의 batch 액션 구현 (#111)

--- app/controllers/external_channels_controller.rb
+++ app/controllers/external_channels_controller.rb
@@ -1,9 +1,17 @@
-class ExternalChannelsController < ApplicationController
+class ExternalChannelsController < ApiController
   def code
     render status: 200, plain: 'get code success'
   end
 
   def batch
+    adapter = ExternalChannel::AdapterFactory.get_adapter(batch_params[:channel_name])
+    manager = ExternalChannel::ManagerFactory.get_manager(batch_params[:type], adapter)
 
+    manager.save_all(batch_params[:query_hash])
+  end
+
+  private
+  def batch_params
+    params.permit(:type, :channel_name, :query_hash)
   end
 end
