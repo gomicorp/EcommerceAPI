e7a60a9aecafec0b6b4c801c9e70ca2161b137b9
JW <jjwoo93@naver.com>
2020-11-17 21:41:20 +0900

    수정: cors 2차 수정

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins(
-        '*',
         'sellers.gomistore.in.th',  # 디딤돌용 셀러즈 모바일앱
         'sellers.gomistore.com',    # 디딤돌용 고미스토어 (+팝업스토어)
         'partners.gomistore.com',   # 디딤돌용 파트너센터
