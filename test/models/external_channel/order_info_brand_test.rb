5459de10d8e9f43bdbbd2186d7c1af4e915b0d7c
Lee seung chan <dltmdcks702@gmail.com>
2020-11-25 11:18:19 +0900

    핫픽스: 이름이 수정된 모델에 대한 후속조치로, 해당 모델을 참조하는 타 모델의 스키마를 수정하는 마이그레이션 생성

--- test/models/external_channel/order_info_brand_test.rb
+++ test/models/external_channel/order_info_brand_test.rb
@@ -2,16 +2,16 @@
 #
 # Table name: external_channel_order_info_brands
 #
-#  id                             :bigint           not null, primary key
-#  created_at                     :datetime         not null
-#  updated_at                     :datetime         not null
-#  brand_id                       :bigint           not null
-#  external_channel_order_info_id :bigint           not null
+#  id            :bigint           not null, primary key
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  brand_id      :bigint           not null
+#  order_info_id :bigint           not null
 #
 # Indexes
 #
 #  brand_index       (brand_id)
-#  order_info_index  (external_channel_order_info_id)
+#  order_info_index  (order_info_id)
 #
 require 'test_helper'
 
