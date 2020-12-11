adefb022b2cb43238b24c6647714456e7a23ebbf
lucas <jjwoo93@naver.com>
2020-12-11 15:37:27 +0900

    핫픽스: CORS에서 화이트리스트를 암호화된 형태에서 가져오도록 수정

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins(
-      'https://partner.gomistore.com',        # = 파트너센터
-      'https://test-partner.gomistore.com',   # = 스테이징 파트너센터
-      'https://vn.gomistore.com',             # = 베트남 고미스토어
-      'https://vn-staging.gomistore.com',     # = 베트남 스테이징 고미스토어
-      'https://gomistore.in.th',              # = 태국 고미스토어
-      'https://staging.gomistore.in.th'       # = 태국 스테이징 고미스토어
+      Rails.application.credentials.dig(:cors_whitelist)
     )
     resource(
       '*',
