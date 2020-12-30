be7c38cd5242e7bcf4ab693981ac23ae1911fd5c
lucas <jjwoo93@naver.com>
2020-12-30 13:31:03 +0900

    수정: with_brand 데코레이터가 사용하는 브랜드 데코레이터의 클래스를 변경

--- app/decorators/companies/with_brand_decorator.rb
+++ app/decorators/companies/with_brand_decorator.rb
@@ -4,6 +4,6 @@ module Companies
 
     # TODO: 기본적으로 자기 원본 레코드의 전체 키를 사용하는 방법을 명시적으로 추가할 필요가 있을 것 같습니다.
     data_keys_from_model :company, except: %i[created_at updated_at]
-    data_key :brands, with: BrandDecorator
+    data_key :brands, with: Brands::DefaultDecorator
   end
 end
