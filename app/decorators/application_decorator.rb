95417741d362ac0a9c072a75eeae4daf5d84165d
lucas <jjwoo93@naver.com>
2020-12-30 17:44:45 +0900

    주석: 어플리케이션 데코레이터에 주석 추가

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,3 +1,7 @@
+# Draper::Decorator 로부터 한 단계 레이어를 추가하기 위한 클래스 입니다.
+# 기본적으로 모델 추상 데코레이터(CompanyDecorator 등)의 부모 클래스로서 동작합니다.
+# 따라서 모든 모델 데코레이터에 대해 동일한 기능을 갖는 메소드들이 모여있게 됩니다.
+
 require_relative './concerns/data_field_collectable'
 
 class ApplicationDecorator < Draper::Decorator
