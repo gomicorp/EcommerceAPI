cf49d8d02bac1d155e392962af3fe2704d60fbce
lucas <jjwoo93@naver.com>
2021-01-11 12:28:43 +0900

    수정: 디딤돌 API의 호스트 명시

--- config/environments/didimdol.rb
+++ config/environments/didimdol.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  Rails.application.routes.default_url_options[:host] = 'didimdol-api.gomistore.com'
+  config.hosts << 'didimdol-api.gomistore.com'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
