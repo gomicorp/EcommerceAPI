957a6ff26cf00bbfe78d046268d2267d1f916770
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2021-01-14 20:12:09 +0900

    수정: slug / 하위호환을 지원해야 함

--- db/data/20210114075545_product_slug_initialize.rb
+++ db/data/20210114075545_product_slug_initialize.rb
@@ -1,7 +1,15 @@
 class ProductSlugInitialize < ActiveRecord::Migration[6.0]
   def up
     ApplicationRecord.country_context_with 'global' do
-      Product.global.find_each(&:save)
+      Product.global.each do |product|
+        locale = product.country.locale
+        title = product.translate(locale: locale).title.presence || product.id
+        product.slug ||= title
+        product.save
+
+        product.slug = title.parameterize.presence || product.id
+        product.save # bomb
+      end
     end
   end
 
