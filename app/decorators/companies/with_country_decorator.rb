d04ffa5b27866a1e5f0851157a309d972f66dd2b
lucas <jjwoo93@naver.com>
2021-01-19 11:53:58 +0900

    수정: 업체+국가 데코레이터 부모클래스 변경

--- app/decorators/companies/with_country_decorator.rb
+++ app/decorators/companies/with_country_decorator.rb
@@ -1,5 +1,5 @@
 module Companies
-  class WithCountryDecorator < CompanyDecorator
+  class WithCountryDecorator < DefaultDecorator
     data_keys_from_model :company, except: %i[created_at updated_at]
     data_key :countries
 
