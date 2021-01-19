fce9a629aed4279831d8ee3ab52eacfdce202eae
JW <jjwoo93@naver.com>
2021-01-19 13:05:45 +0900

    수정: 데코레이터 메소드 위치 이동 및 국가 스코프 해제 (#159)

--- app/decorators/companies/with_brand_country_decorator.rb
+++ app/decorators/companies/with_brand_country_decorator.rb
@@ -2,9 +2,5 @@ module Companies
   class WithBrandCountryDecorator < DefaultDecorator
     data_keys_from_model :company
     data_keys :brands, :countries
-
-    def countries
-      object.brands.map{|brand| brand.country}.uniq
-    end
   end
 end
--- app/decorators/companies/with_country_decorator.rb
+++ app/decorators/companies/with_country_decorator.rb
@@ -2,9 +2,5 @@ module Companies
   class WithCountryDecorator < DefaultDecorator
     data_keys_from_model :company, except: %i[created_at updated_at]
     data_key :countries
-
-    def countries
-      object.brands.map{|brand| brand.country}.uniq
-    end
   end
 end
