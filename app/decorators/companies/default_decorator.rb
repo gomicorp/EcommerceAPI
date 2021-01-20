2223526a03cfcea0394acc68d762112d50b7ca00
lucas <jjwoo93@naver.com>
2021-01-20 17:19:55 +0900

    수정: 모델 메소드에 정상적으로 데코레이팅이 적용되도록 변경

--- app/decorators/companies/default_decorator.rb
+++ app/decorators/companies/default_decorator.rb
@@ -2,12 +2,8 @@ module Companies
   class DefaultDecorator < CompanyDecorator
     data_keys_from_model :company, except: %i[created_at updated_at]
 
-    def brands
-      object.brands
-    end
-
     def countries
-      brands.map{|brand| brand.country}.uniq
+      object.brands.map{|brand| brand.country}.uniq
     end
   end
 end
--- app/decorators/companies/with_brand_country_decorator.rb
+++ app/decorators/companies/with_brand_country_decorator.rb
@@ -2,6 +2,14 @@ module Companies
   class WithBrandCountryDecorator < DefaultDecorator
     data_keys_from_model :company
     data_key :brands, with: Brands::DefaultDecorator
-    data_key :countries, with: Countries::DefaultDecorator
+    data_key :countries
+
+    def countries
+      if super.count > 1
+        Countries::DefaultDecorator.decorate_collection(super)
+      else
+        Countries::DefaultDecorator.decorate(super)
+      end
+    end
   end
 end
