ec8abbb8639894c5be5246aca5d76d48c177ad01
JW <jjwoo93@naver.com>
2021-01-20 17:20:43 +0900

    수정: 모델 메소드에 정상적으로 데코레이팅이 적용되도록 변경 (#164)

--- app/decorators/companies/with_country_decorator.rb
+++ app/decorators/companies/with_country_decorator.rb
@@ -1,6 +1,14 @@
 module Companies
   class WithCountryDecorator < DefaultDecorator
     data_keys_from_model :company, except: %i[created_at updated_at]
-    data_key :countries, with: Countries::DefaultDecorator
+    data_key :countries
+
+    def countries
+      if super.count > 1
+        Countries::DefaultDecorator.decorate_collection(super)
+      else
+        Countries::DefaultDecorator.decorate(super)
+      end
+    end
   end
 end
