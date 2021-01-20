5ee6c9526227161cd4989bc15afe2e4534577a79
lucas <jjwoo93@naver.com>
2021-01-20 12:34:26 +0900

    수정: Country 데코레이터 적용

--- app/decorators/users/managers/with_company_decorator.rb
+++ app/decorators/users/managers/with_company_decorator.rb
@@ -2,7 +2,7 @@ module Users
   module Managers
     class WithCompanyDecorator < DefaultDecorator
       data_keys_from_model :manager
-      data_key :companies
+      data_key :companies, with: Companies::DefaultDecorator
     end
   end
 end
