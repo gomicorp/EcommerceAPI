0ed12ed67b506fbfafcb002e11b7379ea0037258
Bran <40688044+june20516@users.noreply.github.com>
2021-01-22 21:34:00 +0900

    추가: STORE-550/배송 정보 자동 업데이트 코드 최신화

--- app/models/ship_info.rb
+++ app/models/ship_info.rb
@@ -22,17 +22,33 @@
 #
 class ShipInfo < ApplicationRecord
 
-  STATUS = %w[
+  FORWARD_STATUS = %w[
     ship_prepare
     ship_ing
     ship_complete
+  ].freeze
+  BACKWORD_STATUS = %w[
     return_request
     return_processing
     return_complete
   ].freeze
+  STATUS = (FORWARD_STATUS + BACKWORD_STATUS).freeze
   act_as_status_loggable status_list: STATUS.to_echo
 
   enum ship_type: %i[normal express]
+
+  CARRIERS = {
+    alphafast: { name: 'alphaFast', url: 'https://www.alphafast.com/track', trackable: true },
+    '800bestex': { name: 'Best Express', url: 'https://www.best-inc.co.th/track', trackable: true },
+    'cj-korea-thai': { name: 'CJ Express', url: 'https://standardexpress.online/cj-logistics/', trackable: true },
+    flash_express: { name: 'flash_express', url: 'https://www.flashexpress.co.th/tracking/', trackable: false },
+    'j&t_express': { name: 'j&t_express', url: 'https://www.jtexpress.co.th/index/query/gzquery', trackable: false },
+    'kerry-logistics': { name: 'Kerry Express', url: 'https://th.kerryexpress.com/th/track/', trackable: true },
+    'ninjavan-thai': { name: 'Ninja Van', url: 'https://www.ninjavan.co/th-th/tracking', trackable: true },
+    'thailand-post': { name: 'Thai Post', url: 'https://track.thailandpost.co.th/', trackable: true },
+  }.freeze
+
+  enum carrier_code: [*CARRIERS.keys].map(&:to_s).to_echo
 
   FEE_TABLE = {
     normal: 30,
@@ -47,6 +63,7 @@ class ShipInfo < ApplicationRecord
   validates_presence_of :loc_state, :loc_city, :loc_detail
 
   scope :order_status, ->(status_name) { includes(:order_info).where(order_info: OrderInfo.stage_in(status_name)) }
+  scope :auto_trackable, -> { where(carrier_code: self.class::CARRIERS.filter { |_k, v| v[:trackable] }.keys) }
 
   def self.available_status
     STATUS
@@ -63,4 +80,9 @@ class ShipInfo < ApplicationRecord
   def address
     [loc_detail, loc_city, loc_state].join(', ')
   end
+
+  def trackable?
+    self.class::CARRIERS.dig(carrier_code.to_sym, :trackable)
+  end
+
 end
