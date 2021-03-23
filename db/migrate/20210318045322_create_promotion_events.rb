6df36a225422fdd1448b4fe3c569bf2c35adfeca
Robin Kim <robin@gomicorp.com>
2021-03-23 18:11:59 +0900

    기능: 프로모션 이벤트 모델 수정
    - 배경 컬러 추가
    - null, default 옵션 추가

--- db/migrate/20210318045322_create_promotion_events.rb
+++ db/migrate/20210318045322_create_promotion_events.rb
@@ -1,9 +1,10 @@
 class CreatePromotionEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :promotion_events do |t|
-      t.string :name
-      t.datetime :published_at
-      t.datetime :expired_at
+      t.string :name, null: false
+      t.string :background_color, null: false, default: '#333333'
+      t.datetime :published_at, null: false
+      t.datetime :expired_at, null: false
 
       t.timestamps
     end
