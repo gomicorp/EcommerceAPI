ce9dd96dbf3490937bccd592a376cb5ad8f1d9a8
Robin Kim <robin@gomicorp.com>
2021-03-23 22:11:16 +0900

    수정: 프로모션 이벤트 컬럼 이름 수정 name->title

--- db/migrate/20210318045322_create_promotion_events.rb
+++ db/migrate/20210318045322_create_promotion_events.rb
@@ -1,7 +1,7 @@
 class CreatePromotionEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :promotion_events do |t|
-      t.string :name, null: false
+      t.string :title, null: false
       t.string :background_color, null: false, default: '#333333'
       t.datetime :published_at, null: false
       t.datetime :expired_at, null: false
