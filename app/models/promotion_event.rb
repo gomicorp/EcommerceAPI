43f910c39b6e4dece71ce41191a390da7a3cf062
Robin Kim <robin@gomicorp.com>
2021-03-24 19:27:57 +0900

    수정: 프로모션 이벤트에 국가정보, href 정보 추가

--- app/models/promotion_event.rb
+++ app/models/promotion_event.rb
@@ -2,16 +2,28 @@
 #
 # Table name: promotion_events
 #
-#  id           :bigint           not null, primary key
-#  expired_at   :datetime
-#  name         :string(255)
-#  published_at :datetime
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id               :bigint           not null, primary key
+#  background_color :string(255)      default("#333333"), not null
+#  expired_at       :datetime         not null
+#  href             :string(255)      default("")
+#  published_at     :datetime         not null
+#  title            :string(255)      not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  country_id       :bigint           not null
 #
-class PromotionEvent < ApplicationRecord
+# Indexes
+#
+#  index_promotion_events_on_country_id  (country_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (country_id => countries.id)
+#
+class PromotionEvent < NationRecord
   extend_has_one_attached :thumbnail
-  extend_has_one_attached :banner
+  extend_has_one_attached :banner_pc
+  extend_has_one_attached :banner_mb
 
   has_many :event_section_connections, class_name: 'Store::SectionConnection'
   has_many :event_sections, class_name: 'Store::Section', through: :event_section_connections, dependent: :destroy
--- db/migrate/20210318045322_create_promotion_events.rb
+++ db/migrate/20210318045322_create_promotion_events.rb
@@ -1,12 +1,18 @@
 class CreatePromotionEvents < ActiveRecord::Migration[6.0]
-  def change
+  def up
     create_table :promotion_events do |t|
       t.string :title, null: false
       t.string :background_color, null: false, default: '#333333'
+      t.string :href, default: ''
       t.datetime :published_at, null: false
       t.datetime :expired_at, null: false
+      t.references :country, foreign_key: true, null: false
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :promotion_events
   end
 end
