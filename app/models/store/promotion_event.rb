97b82a3cfd1a284ba1d85ab8d121c9aefcb3b4b6
Bran <40688044+june20516@users.noreply.github.com>
2021-04-01 13:47:45 +0900

    기능: STORE-1035/ 이벤트 페이지에 슬러그 기능을 위한 컬럼 추가 (#201)

--- app/models/store/promotion_event.rb
+++ app/models/store/promotion_event.rb
@@ -20,15 +20,19 @@
 #
 #  fk_rails_...  (country_id => countries.id)
 #
-class PromotionEvent < NationRecord
-  extend_has_one_attached :thumbnail
-  extend_has_one_attached :banner_pc
-  extend_has_one_attached :banner_mb
+module Store
+  class PromotionEvent < NationRecord
+    extend FriendlyId
+    extend_has_one_attached :thumbnail
+    extend_has_one_attached :banner_pc
+    extend_has_one_attached :banner_mb
+    friendly_id :title, use: %i[slugged finders history]
 
-  has_many :event_section_connections, class_name: 'Store::SectionConnection'
-  has_many :event_sections, class_name: 'Store::Section', through: :event_section_connections, dependent: :destroy
+    has_many :event_section_connections, class_name: 'Store::SectionConnection'
+    has_many :event_sections, class_name: 'Store::Section', through: :event_section_connections, dependent: :destroy
 
-  has_many :sections, class_name: 'Store::Section', as: :attachable
+    has_many :sections, class_name: 'Store::Section', as: :attachable
 
-  scope :active, ->(standard_time = Time.zone.now) { where('published_at < :now AND expired_at > :now', now: standard_time) }
+    scope :active, ->(standard_time = Time.zone.now) { where('published_at < :now AND expired_at > :now', now: standard_time) }
+  end
 end
