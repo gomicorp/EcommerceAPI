f74aac17b6fdbcdb0a7c9a2623201ad9c883de95
Robin Kim <robin@gomicorp.com>
2021-04-23 11:51:22 +0900

    기능: STORE-1096/ OrderConfirmation 모델 만들기

---
+++ app/models/order_confirmation.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: order_confirmations
+#
+#  id            :bigint           not null, primary key
+#  contact_count :integer
+#  memo          :text(65535)
+#  status        :string(255)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  order_info_id :bigint           not null
+#
+# Indexes
+#
+#  index_order_confirmations_on_order_info_id  (order_info_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (order_info_id => order_infos.id)
+#
+class OrderConfirmation < ApplicationRecord
+end
---
+++ db/migrate/20210423024251_create_order_confirmations.rb
@@ -0,0 +1,12 @@
+class CreateOrderConfirmations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :order_confirmations do |t|
+      t.references :order_info, foreign_key: true, null: false
+      t.integer :contact_count
+      t.text :memo
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
