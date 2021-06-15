a8d93a5135a329670cbbc35795e6290e859e5e74
Robin Kim <robin@gomicorp.com>
2021-06-15 18:43:29 +0900

    [refactor] 현재 payment에 charge 모델을 설정하는 마이그레이션 파일 수정
    paid가 nil인 주문이 있어서 마이그레이션이 불가한 것을 고치기 위한 코드입니다.
    프로덕션 환경에서 해당하는 레코드는 1건입니다. id: 4379

--- db/data/20210610045910_set_charge_for_current_payment.rb
+++ db/data/20210610045910_set_charge_for_current_payment.rb
@@ -7,6 +7,8 @@ class SetChargeForCurrentPayment < ActiveRecord::Migration[6.0]
       Payment::Charge.create(payment: payment, pg_name: pg_name, external_charge_id: pg_id, status: Payment::Charge.statuses[:pending], created_at: payment.created_at)
       Payment::Charge.create(payment: payment, pg_name: pg_name, external_charge_id: pg_id, status: Payment::Charge.statuses[:paid], created_at: payment.paid_at)  if payment.paid
 
+      # paid가 nil인 주문이 있어서 마이그레이션이 불가한 것을 고치기 위한 코드. 프로덕션 환경에서 해당하는 레코드는 1건입니다. id: 4379
+      payment.update(paid: false, expire_at: (payment.created_at + 30.minutes)) if payment.paid.nil?
       Payment::Charge.create(payment: payment, pg_name: pg_name, external_charge_id: pg_id, status: Payment::Charge.statuses[:expired], created_at: payment.expire_at)  if !payment.paid && payment.expire_at < now
       Payment::Charge.create(payment: payment, pg_name: pg_name, external_charge_id: pg_id, status: Payment::Charge.statuses[:refunded], created_at: Payment.last.order_info&.items&.first&.cancelled_tag&.created_at || payment.updated_at)  if payment.paid && payment.cancelled
     end
